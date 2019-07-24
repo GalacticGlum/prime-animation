0ae47cbd233e900e3eb51dad27f60a5eb40c2f22
GalacticGlum <nerftactic09@gmail.com>
2019-07-24 10:47:52 -0400

    Added SASS loader to webpack config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import p5 from 'p5';
 import 'p5/lib/addons/p5.sound';
 import 'p5/lib/addons/p5.dom';
+import './scss/index.scss';
 
 import sketch from './sketch';
 
--- webpack.config.js
+++ webpack.config.js
@@ -26,13 +26,43 @@ module.exports = {
         ]
     },
     module: {
-        rules: [{
-            test: /\.js/,
-            exclude: /(node_modules|bower_components)/,
-            use: [{
-                loader: 'babel-loader'
-            }]
-        }]
+        rules: [
+            {
+                test: /\.js/,
+                exclude: /(node_modules|bower_components)/,
+                use: [{
+                    loader: 'babel-loader'
+                }]
+            },
+            {
+                test: /\.(scss)$/,
+                use: [
+                    {
+                        // Adds CSS to the DOM by injecting a <style> tag
+                        loader: 'style-loader'
+                    },
+                    {
+                        // Interprets @import and url() like import/require() and will resolve them
+                        loader: 'css-loader'
+                    },
+                    {
+                        // Loader for webpack to process CSS with PostCSS
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: function() {
+                                return [
+                                    require('autoprefixer')
+                                ];
+                            }
+                        }
+                    },
+                    {
+                        // Loads a SASS/SCSS file and compiles it to CSS
+                        loader: 'sass-loader'
+                    }
+                ]
+            }
+        ]
     },
     plugins: [
         new CleanWebpackPlugin(),
